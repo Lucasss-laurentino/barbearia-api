9905e9fc2a10cc069ac19f55fc929b4fd0232b16
Lucas <Lucaselorenzohenrique@gmail.com>
2022-11-21 13:11:13 -0300

    login, create e index

---
+++ laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function createUser(Request $request) {
+        
+        $user = User::create([
+            'email' => $request->email,
+            'password' =>  Hash::make($request->password),
+        ]);
+    
+        return $user;    
+        
+    }
+
+    public function login(Request $request) {
+
+        $credentials = ['email' => $request->email, 'password' => $request->password];
+
+        if(Auth::attempt($credentials)) {
+
+            $user = Auth::user();
+
+            $token = $user->createToken('token');
+
+            return [$user, $token->plainTextToken];
+        
+        } else {
+        
+            return 'erro';
+        
+        }
+
+    }
+
+    public function index() {
+        // Retornar todos barbeiros e horarios de cada barbeiro
+
+        return 'voce esta no index';
+    }
+}
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Routes for authenticated
+Route::middleware('auth:sanctum')->group(function() {
+
+    Route::get('/index', [UserController::class, 'index']);
+
+});
+
+// Routes publics
+Route::post('/createUser', [UserController::class, 'createUser']);
+Route::post('/login', [UserController::class, 'login']);
