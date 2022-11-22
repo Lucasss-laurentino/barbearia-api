6b2ca9fff5858983dfb455ea7c7f96d867ad169e
Lucas <Lucaselorenzohenrique@gmail.com>
2022-11-22 07:31:27 -0300

    Rota e metodo criar barbeiro

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdmController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware('auth:sanctum')->group(function() {
 
     Route::get('/index', [UserController::class, 'index']);
+    Route::post('/createBarber', [AdmController::class, 'createBarber']);
 
 });
 
